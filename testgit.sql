c96884fb9b61fd951759153353b51077236cd614
jp <liquidsun@wanadoo.fr>
2014-06-25 12:37:42 +0200

    testgit

--- testgit.sql
+++ testgit.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Mer 25 Juin 2014 à 12:29
+-- Généré le :  Mer 25 Juin 2014 à 12:37
 -- Version du serveur :  5.6.17
 -- Version de PHP :  5.5.12
 
@@ -30,6 +30,18 @@ CREATE TABLE IF NOT EXISTS `aaa` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `num` int(11) NOT NULL,
   PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `bbb`
+--
+
+CREATE TABLE IF NOT EXISTS `bbb` (
+  `idbbb` int(11) NOT NULL AUTO_INCREMENT,
+  `jhgjhg` int(11) NOT NULL,
+  PRIMARY KEY (`idbbb`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
