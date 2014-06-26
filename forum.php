3946d781cd5e561be6db625797fc675ec3223ad3
jp <liquidsun@wanadoo.fr>
2014-06-26 12:24:59 +0200

    ajout des pages en construction

---
+++ forum.php
@@ -0,0 +1,24 @@
+<?php
+	session_start();
+?>
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<title> forum </title>
+		<link href="css/style.css" media="all" rel="stylesheet" type="text/css" />
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	</head>
+	
+	<body>
+		<?php
+			include('php/header.php');
+		?>
+		<div class="logo"></div>
+		<h1> page forum en construction</h1>
+		<?php
+			include('php/footer.php');
+		?>
+	</body>
+</html>
+	
