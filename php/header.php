37cfdaec08e8b0b475fef4dfe9361cfe6f1760f3
Olivier Borel <borel-olivier@live.fr>
2014-06-26 12:48:58 +0200

    test

--- php/header.php
+++ php/header.php
@@ -1,12 +1,25 @@
 <header>
 	<?php //include('');
+	if(!$_SESSION) {
 	?>
-	<form method="POST" action="php/connexion.php">
-		<label for="input_login">Identifiant : </label>
-		<input type="text" id="input_login" name="user_login" />
-		<label for="input_password">Mot de passe : </label>
-		<input type="password" id="input_login" name="user_password" />
-		<input type="submit" value="OK" />
-		<a href="#">mot de passe oublié ?</a>
-	</form>
+		<form method="POST" action="php/connexion.php">
+			<label for="input_login">Identifiant : </label>
+			<input type="text" id="input_login" name="user_login" />
+			<label for="input_password">Mot de passe : </label>
+			<input type="password" id="input_login" name="user_password" />
+			<input type="submit" value="OK" />
+			<a href="#">mot de passe oublié ?</a>	
+		</form>
+    <?php
+    }
+
+	else {
+    ?>
+        <form method="POST" action="php/deconnexion.php">
+        	<input type="submit" value="Deconnexion" />
+        </form>
+    <?php
+    }
+include('menu.php');
+	?>
 </header>
