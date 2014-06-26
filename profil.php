7afa593bf4bf55580984a121e194743c53e3be6b
jp <liquidsun@wanadoo.fr>
2014-06-26 11:58:57 +0200

    ajout page profil et style css logo

---
+++ profil.php
@@ -0,0 +1,24 @@
+<?php
+	session_start();
+?>
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<title> profil </title>
+		<link href="css/style.css" media="all" rel="stylesheet" type="text/css" />
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	</head>
+	
+	<body>
+		<?php
+			include('php/header.php');
+		?>
+		<div class="logo"></div>
+		<h1>page profil en construction</h1>
+		<?php
+			include('php/footer.php');
+		?>
+	</body>
+</html>
+	
